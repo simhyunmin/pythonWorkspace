cbc3a3726260bd35ac0f7e7f9a016c67d5652746
simhyunmin <jpsim1234@naver.com>
2022-06-30 00:58:30 +0900

    파이썬

---
+++ baekjoon/5622.py
@@ -0,0 +1,32 @@
+# times = 3
+# dial = [i for i in range(1, 27)]
+
+# for i in range(0, 8):
+#   times = 3+i
+#   for j in range(3*i,3*i+3 ):
+#     dial[j] = times
+# for i in range(15, 19):
+#   dial[i] = 8
+# for i in range(20, 22):
+#   dial[i] = 9
+# for i in range(23, 26):
+#   dial[i] = 10
+# idx = list(input())
+# sum = 0
+# for i in idx:
+#   sum = sum+dial[ord(i)-ord("A")]
+# print(sum)
+
+
+
+# word = list(input())
+# time = 0
+# num_str = list("ABC DEF GHI JKL MNO PQRS TUV WXYZ".split())
+
+# for i in word:
+#     for j in num_str:
+#         if i in j:
+#             time += 3 + num_str.index(j)
+
+
+# print(time)
--- baekjoon/tempCodeRunnerFile.py
+++ baekjoon/tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-han = han+1
+for i in range(20, 23):
+#   dial[i] = 9
+# for i in range(24, 26):
+#   dial[i] = 10
